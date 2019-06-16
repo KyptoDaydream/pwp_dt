5df7da0bbad508dc2c51a94b168bb020ccc6af76
Erik Schwarz <Erik.Schwarz@performgroup.com>
2019-06-16 22:55:22 +0200

    style update

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,6 +3,8 @@ import React from "react"
 import Layout from "../components/layout404"
 import SEO from "../components/seo"
 
+import "../styles/global.css"
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
--- src/pages/hurray.js
+++ src/pages/hurray.js
@@ -3,6 +3,8 @@ import React from "react"
 import Layout from "../components/layoutHurray"
 import SEO from "../components/seo"
 
+import "../styles/global.css"
+
 const Hurray = () => (
   <Layout>
     <SEO title="HURRAY! Ozvem sa čoskoro" />
