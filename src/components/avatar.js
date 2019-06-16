0042241a5f91f1eea6b35ea0e911d351c30876bb
Erik Schwarz <Erik.Schwarz@performgroup.com>
2019-06-16 17:26:51 +0200

    update

--- src/components/avatar.js
+++ src/components/avatar.js
@@ -1,18 +1,15 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
-
 import bushido_avatar from "../images/bushido_avatar_3.svg"
-import bushido_avatar_white from "../images/bushido_avatar_3_white.svg"
-
-
+import bushido_avatar_white from "../images/bushido_avatar_3_white_2.svg"
 
 const AvatarImage = styled.div`
   position: absolute;
   top: 50px;
   left: 50px;
-  width: 100px;
-  height: 100px;
+  width: 120px;
+  height: 120px;
   background: url(${props => props.color ? bushido_avatar_white : bushido_avatar});
   transition: 0.3s;
   z-index: 9999;
